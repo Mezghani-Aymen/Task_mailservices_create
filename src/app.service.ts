aa650f43b2ab39ed448f49cba10f053c6942fbb9
Mohamed Fawzi Abdellaoui <129664741+MohamedFawziAbdellaoui@users.noreply.github.com>
2024-03-08 21:25:27 +0100

    Add comments app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  // Delete method and add the service method for the checkserverhealth to be used in the controller 
   getHello(): string {
     return 'Hello World!';
   }
