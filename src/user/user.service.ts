2f20e5a9149856c5ddb987bfa1357dae348f454d
MzAymen <mezghanimohamedaymen@gmail.com>
2024-03-14 14:23:47 +0100

    [add] try_catch user.servie

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -16,29 +16,43 @@ export class UserService {
   ) {}
 
   async getAllUsers(): Promise<User[]> {
-    const users = await this.userModel.find().exec();
-    return users;
+    try {
+      const users = await this.userModel.find().exec();
+      return users;
+    } catch (error) {}
   }
 
   async findUser(numphone: string): Promise<User | null> {
-    const user = await this.userModel.findOne({ phone: numphone }).exec();
-    return user ? user : null;
+    try {
+      const user = await this.userModel.findOne({ phone: numphone }).exec();
+      return user ? user : null;
+    } catch (error) {}
   }
 
   async createUser(userdata: UserDto): Promise<User> {
-    const res = await this.userModel.create(userdata);
-    return res;
+    try {
+      const res = await this.userModel.create(userdata);
+      return res;
+    } catch (error) {}
   }
 
   async deleteUser(phone: string) {
-    await this.userModel.findOneAndDelete({ phone: `${phone}` });
+    try {
+      await this.userModel.findOneAndDelete({ phone: `${phone}` });
+    } catch (error) {}
   }
 
   async updateUser(phone: string, data: UserDto): Promise<User> {
-    const updateUser = await this.userModel.findOneAndUpdate({ phone }, data, {
-      new: true,
-      runValidators: true,
-    });
-    return updateUser;
+    try {
+      const updateUser = await this.userModel.findOneAndUpdate(
+        { phone },
+        data,
+        {
+          new: true,
+          runValidators: true,
+        },
+      );
+      return updateUser;
+    } catch (error) {}
   }
 }
