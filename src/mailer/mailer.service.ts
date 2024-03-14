9d8cb7f0764a1a66d77a3793209ed9ed2f8eaf8c
MzAymen <mezghanimohamedaymen@gmail.com>
2024-03-14 21:08:59 +0100

    [fix] add readHtmlTemplate and sendEmail

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,13 +1,25 @@
 /* eslint-disable prettier/prettier */
 import { ConfigService } from '@nestjs/config';
 import * as nodemailer from 'nodemailer';
-import { SendEmailDto } from './mail.interface';
 import Mail from 'nodemailer/lib/mailer';
 import { Injectable } from '@nestjs/common';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
 
 @Injectable()
 export class MailerService {
   constructor(private readonly configService: ConfigService) {}
+
+  private async readMailHtml(): Promise<string> {
+    const filePath = join(__dirname, '../..', 'public', 'mail.html');
+    try {
+      const htmlContent = await readFile(filePath, 'utf-8');
+      return htmlContent;
+    } catch (error) {
+      console.error('Error reading mail.html:', error);
+      throw new Error('Failed to read mail.html');
+    }
+  }
 
   template = (html: string, replacements: Record<string, string>) => {
     return html.replace(/%(\w*)%/g, function (match, key) {
@@ -28,21 +40,27 @@ export class MailerService {
     return transporter;
   }
 
-  sendEmail(dto: SendEmailDto) {
-    const { from, recipients, subject } = dto;
-
-    const html = dto.placeholderReplacements
-      ? this.template(dto.html, dto.placeholderReplacements)
-      : dto.html;
-
+  async sendEmail(bodyMail: Record<string, string>) {
+    const dto = {
+      from: {
+        name: this.configService.get<string>('MAIL_SENDER'),
+        address: this.configService.get<string>('EMAIL_SENDER'),
+      },
+      recipients: [{ name: bodyMail.name, address: bodyMail.mail }],
+      subject: bodyMail.subject,
+      html: bodyMail.text,
+    };
+    const htmlContent = await this.readMailHtml();
+    const subject = dto.subject;
+    const html = this.template(htmlContent, { name: bodyMail.name });
     const transport = this.mailTransport();
 
     const options: Mail.Options = {
-      from: from ?? {
+      from: dto.from ?? {
         name: this.configService.get<string>('APP_NAME'),
         address: this.configService.get<string>('DEFAULT_MAIL_FROM'),
       },
-      to: recipients,
+      to: dto.recipients,
       subject,
       html,
     };
@@ -50,8 +68,6 @@ export class MailerService {
     try {
       const result = transport.sendMail(options);
       return result;
-    } catch (error) {
-      return error;
-    }
+    } catch (error) {}
   }
 }
